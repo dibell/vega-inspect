137b144fec59037ccb473900eba76c9995e0f5fd
Dave Bell <david.bell@isotoma.com>
2018-06-23 10:39:39 +0100

    move to content script

--- background.js
+++ background.js
@@ -5,11 +5,14 @@ chrome.runtime.onInstalled.addListener(function() {
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          css: ["#container"]
+        })
 	  ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
+      actions: [
+        new chrome.declarativeContent.ShowPageAction()
+      ]
 	}]);
   });
 });
